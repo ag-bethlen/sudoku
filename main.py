20b4d48360e400c0c6a75b3a4938376696ec9d69
Gergely Árvay <65071263+ag-bethlen@users.noreply.github.com>
2022-01-04 06:57:35 +0000

    v0.1

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
-board = [[0]*9]*9
+import blankboard
+
+class Board(list):
+    def __init__(self, **kwargs):
+        super().__init__(self.load_data(**kwargs))
+        self.size = len(self)
+        self.solved = False
+        self.original = [] # itt folyt.
+        self.valid = []
 
 
-def print_sudoku_board(board):
-  print('┌───┬───┬───┬───┬───┬───┬───┬───┬───┐')
-  for i in range(9):
-    print(end="│ ")
-    for j in range(9):
-      print(board[i][j], end=" │ ")
-    print()
-    if i < 8:
-      print('├───┼───┼───┼───┼───┼───┼───┼───┼───┤')
-  else:
-    print('└───┴───┴───┴───┴───┴───┴───┴───┴───┘')
+    def load_data(self, **kwargs):
+        if kwargs['type'] == 'file':
+            with open(kwargs['data']) as f:
+                return [[5, 8, 2, 1, 3, 0, 4, 6, 9],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0]] # ide a betöltőt meg kell írni
+        elif kwargs['type'] == 'list':
+            return kwargs['data']
 
-print_sudoku_board(board)
+board = Board(type = 'file', data = 'boards/001.txt')
+print(board)
